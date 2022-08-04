c415a2970a4527abe8fb0505d75223ed5aafdcba
leej9233 <leej9233@gmail.com>
2022-08-03 21:43:33 -0500

    SMU Bootcamp Module 3

---
+++ PyBank/main.py
@@ -0,0 +1,100 @@
+#Modules
+import csv
+
+#Reading CSV Module
+with open('Resources/budget_data.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    
+    #Read the hedaer row first
+    header = next(csvreader)
+
+    #CHECK: SKIP HEADER
+    #print(header)
+
+    #List preparation
+    months = []
+    profit_losses = []
+    changes = []
+
+    #Task No.1: The total number of months included in the dataset
+    for row in csvreader:
+        months.append(row[0])
+        profit_losses.append(row[1])
+
+    total_months = len(months)
+
+    #CHECK: TOTAL MONTH
+    #print(total_months)
+
+    total = 0
+
+    #Task No.2: The net total amount of "Profit/Losses" over the entire period    
+    for amount in profit_losses:
+        total = total + int(amount)
+
+    #CHECK: TOTAL PROFIT/LOSS
+    #print(total)
+
+    #Task No.3: The changes in "Profit/Losses" over the entire period, and then the average of those changes
+    for i in range(total_months-1):
+        change = int(profit_losses[i+1])-int(profit_losses[i])
+        changes.append(change)
+
+    #CHECK:CHANGE IN PROLOSS
+    #print(changes)
+
+    total_changes = 0
+
+    for avg_change in changes:
+        total_changes = total_changes + int(avg_change)
+
+    final_changes = round(total_changes/(total_months-1), 2)
+    
+    #CHECK: AVERAGE CHANGE
+    #print(final_changes)
+
+    g_increase = 0
+    g_months = ''
+
+    #Task No.4: The greatest increase in profits (date and amount) over the entire period
+    for j in range(total_months-1):
+        if changes[j] > g_increase:
+            g_increase = changes[j]
+            g_months = months[j+1]
+            
+    #CHECK: MONTH + GREATEST INCREASE IN PROFITS
+    #print(g_months)
+    #print(g_increase)
+
+    g_decrease = 0
+    d_months = ''
+
+    #Task No.5: The greatest decrease in profits (date and amount) over the entire period
+    for j in range(total_months-1):
+        if changes[j] < g_decrease:
+            g_decrease = changes[j]
+            d_months = months[j+1]
+    
+    #CHECK: MONTH + GREATEST DECREASE IN PROFITS
+    #print(d_months)
+    #print(g_decrease)
+
+    #TEXT FILE
+    output_file=open("Analysis/pybank.txt","w")
+    output_file.write("Financial Analysis\n")
+    output_file.write("----------------------------\n")
+    output_file.write("Total Months: "+ str(total_months)+"\n")
+    output_file.write("Total: $"+str(total)+"\n")
+    output_file.write("Average Change: $"+str(final_changes)+"\n")
+    output_file.write("Greatest Increase in Profits: "+str(g_months)+" ($"+str(g_increase)+")\n")
+    output_file.write("Greatest Decrease in Profits: "+str(d_months)+" ($"+str(g_decrease)+")\n")
+    output_file.close()
+
+    #PRINT TO THE TERMINAL
+    print("Financial Analysis")
+    print("----------------------------")
+    print("Total Months: "+ str(total_months)+"")
+    print("Total: $"+str(total)+"")
+    print("Average Change: $"+str(final_changes)+"")
+    print("Greatest Increase in Profits: "+str(g_months)+" ($"+str(g_increase)+")")
+    print("Greatest Decrease in Profits: "+str(d_months)+" ($"+str(g_decrease)+")")
---
+++ PyPoll/main.py
@@ -0,0 +1,100 @@
+#Modules
+import csv
+
+#Reading CSV Module
+with open('Resources/election_data.csv', 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    #Read the hedaer row first
+    header = next(csvreader)
+
+    #CHECK: SKIP HEADER
+    #print(header)
+
+    #List preparation
+    voterid = []
+    country = []
+    candidates = []
+    v_candidates = []
+    ind_votes = []
+    perc_votes = []
+
+    #Task No.1: The total number of votes cast
+    for row in csvreader:
+        voterid.append(row[0])
+        country.append(row[1])
+        candidates.append(row[2])
+
+    total_votes = len(voterid)
+
+    #CHECK:TOTAL VOTES
+    #print(total_votes)
+    
+    #List first candidate in the list
+    v_candidates.append(candidates[0])
+
+    #Task No.2: A complete list of candidates who received votes
+    for recvotes in range(total_votes-1):
+        if candidates[recvotes+1] != candidates[recvotes] and candidates[recvotes+1] not in v_candidates:
+            v_candidates.append(candidates[recvotes+1])
+
+    #CHECK: CANDIDATES
+    #print(v_candidates)
+
+    #Task No.4: The total number of votes each candidate won
+
+    for cvotes in range(len(v_candidates)):
+        ind_votes.append(candidates.count(v_candidates[cvotes]))
+    
+    #CHECK: INDIVIDUAL VOTES
+    #print(ind_votes)
+
+    #Task No.3: The percentage of votes each candidate won
+    for percvotes in range(len(v_candidates)):
+        perc_votes.append(round(((ind_votes[percvotes]/total_votes)*100), 3))
+    
+    #CHECK: VOTE PERCENTAGES
+    #print(perc_votes)
+
+    winner = 0
+    second = 0
+    third = 0
+
+    #Task No.5: The winner of the election based on popular vote
+    second = ind_votes[0]
+    secondname = v_candidates[0]
+    secondperc = perc_votes[0]
+    for votecount in range(len(v_candidates)):
+        if ind_votes[votecount]>second:
+            winner = ind_votes[votecount]
+            winnername = v_candidates[votecount]
+            winnerperc = perc_votes[votecount]
+        elif ind_votes[votecount]<second:
+            third = ind_votes[votecount]
+            thirdname = v_candidates[votecount]
+            thirdperc = perc_votes [votecount]
+
+    #CHECK: Winner/Second/Third Info
+    #print(winner)
+    #print(winnername)
+    #print(winnerperc)
+    #print(second)
+    #print(secondname)
+    #print(secondperc)
+    #print(third)
+    #print(thirdname)
+    #print(thirdperc)
+
+    #TEXT FILE
+    output_file=open("Analysis/pypoll.txt","w")
+    output_file.write("Election Results\n")
+    output_file.write("-------------------------\n")
+    output_file.write("Total Votes: "+ str(total_votes)+"\n")
+    output_file.write("-------------------------\n")
+    output_file.write(str(v_candidates[0])+": "+ str(perc_votes[0])+"% ("+str(ind_votes[0])+")\n")
+    output_file.write(str(v_candidates[1])+": "+ str(perc_votes[1])+"% ("+str(ind_votes[1])+")\n")
+    output_file.write(str(v_candidates[2])+": "+ str(perc_votes[2])+"% ("+str(ind_votes[2])+")\n")
+    output_file.write("-------------------------\n")
+    output_file.write("Winner: "+str(winnername)+"\n")
+    output_file.write("-------------------------\n")
+    output_file.close()
